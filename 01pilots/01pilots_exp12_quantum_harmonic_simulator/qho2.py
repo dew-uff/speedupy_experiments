406a7d7dc58f9ff7e6c7777e4fd2e2c6304d59aa
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2023-08-29 06:26:48 -0300

    Update qho2.py

--- 01pilots/01pilots_exp12_quantum_harmonic_simulator/qho2.py
+++ 01pilots/01pilots_exp12_quantum_harmonic_simulator/qho2.py
@@ -9,14 +9,14 @@ FONTES: https://docs.scipy.org/doc/numpy/reference/generated/numpy.zeros.html
 """
 
 from __future__ import division
-from scipy.linalg import eigh
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.tri import Triangulation
 import numpy as np
-import matplotlib.pyplot as plt
+import time, sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / "speedupy"))
 
-#E DETERMINISTICA
+from intpy import initialize_intpy, deterministic
+
+@deterministic
 def coulomb_matrix(n):
     """
     Compute the Coulomb interaction matrix which has matrix elements
@@ -72,7 +72,7 @@ def coulomb_matrix(n):
 
     return C
 
-#E DETERMINISTICA
+@deterministic
 def hermite_functions(x,n):
     """
     Evaluate the first n normalized Hermite functions on a grid x
@@ -89,54 +89,18 @@ def hermite_functions(x,n):
 
     return psi 
 
-
-
+@initialize_intpy(__file__)
+def main(x,n):
+    t0 = time.perf_counter()
+    hf = hermite_functions(x,2*n+1)
+    t1 = time.perf_counter()
+    C = coulomb_matrix(n)
+    t2 = time.perf_counter()
+    print(t1-t0)
+    print(t2-t1)
 
 if __name__ == '__main__':
-
-    x = np.linspace(-10,10,100)
-    yy,xx = np.meshgrid(x,x)
-
-    # Original coordinate system
-    Q1 = (xx+yy)/np.sqrt(2)
-    Q2 = (xx-yy)/np.sqrt(2)
-
-    n = 16
-    
-    # Compute Hermite functions
-    hf = hermite_functions(x,2*n+1)
- 
-    # Use anti-symmetric functions as basis
-    phi = hf[:,1::2] 
-
-    # Non-Coulombic part of Hamiltonian H0 = (K+Q)/2
-    H0 = np.diag((3+4*np.arange(n))/2)
-
-    # Coulombic part of Hamiltonian
-    C = coulomb_matrix(n)
-
-    # Total Hamiltonian 
-    H = H0 + C 
-
-    nu,Vhat = eigh(H)
-    
-    # Evaluate antisymmetric Hermite-function series on grid 
-    V = np.dot(phi,Vhat)
-
-    # Create an eigenfunction to the two-particle problem
-    Psi = np.outer(hf[:,0],V[:,0]) 
-   
-    psi = Psi.flatten() 
-    q1 = Q1.flatten()
-    q2 = Q2.flatten()
-
-    dex = np.where((q1**2+q2**2)<10)[0]
-    tri = Triangulation(q2[dex],q1[dex])
-    fig = plt.figure(1)
-    ax =  fig.gca(projection='3d')
-    ax.plot_trisurf(tri,psi[dex], cmap=cm.jet, linewidth=0.2 ) 
-    ax.set_xlabel(r'$q_1$',fontsize=18)
-    ax.set_ylabel(r'$q_2$',fontsize=18)
-    plt.show(block=False)
-
-  
+    length = int(sys.argv[1])
+    n = int(sys.argv[2])
+    x = np.linspace(-length,length,100)
+    main(x,n)
