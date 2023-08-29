36bd7711622a34aa8343fba8823d96914ab24431
Clayton Escouper das Chagas <claytonchagas1@gmail.com>
2023-08-29 06:35:32 -0300

    Update test_belief_propagation.py

--- 04benchproglangs/04benchpl_exp06_belief_propagation_OK/test_belief_propagation.py
+++ 04benchproglangs/04benchpl_exp06_belief_propagation_OK/test_belief_propagation.py
@@ -1,10 +1,12 @@
-
 import numpy as np
 import sys
 
 import time
 
-from intpy.intpy import initialize_intpy, deterministic
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / "speedupy"))
+
+from intpy import initialize_intpy, deterministic
 
 @deterministic
 def belief_propagation(N):
