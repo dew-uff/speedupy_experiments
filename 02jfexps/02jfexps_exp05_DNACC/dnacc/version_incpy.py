d389e66529f14c52ff47845232a63a39a25f16c2
JoaoLopez <joaolopez@id.uff.br>
2023-10-02 17:40:10 -0300

    Adicionando seis experimentos da planilha do João Felipe que conseguiram ser executados com o IncPy. (Tiny GSGP, Digital RADs, Diversity Simulation, Twitter NLP, DNACC e RiboDiff)

---
+++ 02jfexps/02jfexps_exp05_DNACC/dnacc/version_incpy.py
@@ -0,0 +1,9 @@
+# THIS FILE IS GENERATED FROM DNACC SETUP.PY
+short_version = '1.1.0'
+version = '1.1.0'
+full_version = '1.1.0.dev-71a5390'
+git_revision = '71a53901362790a0d14151ad0a7d4e3b832a8538'
+release = False
+
+if not release:
+    version = full_version
