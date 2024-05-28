a2af3cc8f5639ed9246c74bc83f305ad4e9bc7b7
JoaoLopez <joaolopez@id.uff.br>
2024-05-28 17:35:51 -0300

    Adapting eq_solver.py script to execute with speedupy

--- 05msrgithubexps/05msrgithubexps_exp03_eq_solver/eq_solver.py
+++ 05msrgithubexps/05msrgithubexps_exp03_eq_solver/eq_solver.py
@@ -22,19 +22,19 @@ def solve(n, F_solution):
     return fsolve(func=lambda x: n * C / (1 + Kd1 / x) + n2 * C / (1 + Kd2 / x) - T, x0=F_solution, xtol=1e-6)
 
 @deterministic
-def main(F_solution):
+def exp_main(F_solution):
     for i in range(n_solutions):
         # Define function
         F_solution=solve(x_list[i], F_solution)
         F_list[i]=F_solution
 
 @initialize_speedupy
-def exp_main():
+def main():
     F_solution = int(sys.argv[1])
     F_solution = F_solution/10
     start = time.perf_counter()
-    main(F_solution)
+    exp_main(F_solution)
     print(time.perf_counter() - start)
 
 if __name__ == '__main__':
-    exp_main()
+    main()
