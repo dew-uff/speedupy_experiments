cb16820664bb460efcedf7b63ecf629c079d560b
JoaoLopez <joaolopez@id.uff.br>
2024-06-10 19:48:07 -0300

    Removing 02jfexps_exp04_Twitter_NLP/ experiment because we could not reproduce it on Phoenix

--- 02jfexps/02jfexps_exp04_Twitter_NLP/hbc/models/add_fflush.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-cat $1 | perl -0 -ne 's/printf\("\\n"\);/printf("\\n"); fflush(stdout);/g; print;' > $1-tmp
-mv $1-tmp $1
--- 02jfexps/02jfexps_exp04_Twitter_NLP/python/cap/build.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/../../lib
-c++ -I. -static -L../../lib cap_classify.cpp -o cap_classify -ltinysvm
