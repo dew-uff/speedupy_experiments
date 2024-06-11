10adb52c3ed2413a9e16255bb93ddebf5cca53e9
JoaoLopez <joaolopez@id.uff.br>
2024-06-11 17:59:41 -0300

    Adapting experiment script to better use memoization tools

--- 04benchproglangs/04benchpl_exp03_count_unique_words_OK/count_unique_words_py3.py
+++ 04benchproglangs/04benchpl_exp03_count_unique_words_OK/count_unique_words_py3.py
@@ -11,18 +11,26 @@ def strip_word(word):
                     punctuation_characters]).strip('\"').lower()
 
 def count_words_dictionary(file_name):
+    data = open(file_name).read()
+    return process_count_words_dictionary(data)
+
+def process_count_words_dictionary(data):
     dictionary = defaultdict(int)
-    for word in open(file_name).read().split():
+    for word in data.split():
         dictionary[strip_word(word)] += 1
     del dictionary['']
     return len(dictionary)
 
 def count_words_set(file_name):
     with open(file_name, "r") as file_id:
-        lines = file_id.read().splitlines()
-        uniques = set()
-        for line in lines:
-            uniques |= set(strip_word(m) for m in line.split())
+        data = file_id.read()
+        return process_count_words_set(data)
+
+def process_count_words_set(data):
+    lines = data.splitlines()
+    uniques = set()
+    for line in lines:
+        uniques |= set(strip_word(m) for m in line.split())
     uniques.remove('')
     #print(uniques)
     return len(uniques)
@@ -38,8 +46,10 @@ def main():
     file_name = sys.argv[1]
     t0 = time.perf_counter()
     n = count_words_dictionary(file_name)
+    print(n)
     t1 = time.perf_counter()
     n = count_words_set(file_name)
+    print(n)
     t2 = time.perf_counter()
     print(t1-t0)
     print(t2-t1)
