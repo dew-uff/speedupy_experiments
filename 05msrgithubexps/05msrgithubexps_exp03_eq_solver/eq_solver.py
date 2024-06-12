ccec51e2d3b8b46f90dcb81c794cde6098f3fc13
JoaoLopez <joaolopez@id.uff.br>
2024-06-12 18:45:11 -0300

    Adapting experiment script to better use memoization tools

--- 05msrgithubexps/05msrgithubexps_exp03_eq_solver/eq_solver.py
+++ 05msrgithubexps/05msrgithubexps_exp03_eq_solver/eq_solver.py
@@ -4,29 +4,26 @@ from scipy.optimize import fsolve
 import time
 import sys
 from speedupy.speedupy import initialize_speedupy, deterministic
-# Define parameters
-
-T = 1
-C = 1
-Kd1 = 2
-n2 = 1000
-Kd2 = 1
-
-# Use the numerical solver to find the roots
-n_solutions = 200
-x_list = np.linspace(1, 2e4, n_solutions)
-F_list = np.empty_like(x_list)
 
 @deterministic
 def solve(n, F_solution):
+    T = 1
+    C = 1
+    Kd1 = 2
+    n2 = 1000
+    Kd2 = 1
     return fsolve(func=lambda x: n * C / (1 + Kd1 / x) + n2 * C / (1 + Kd2 / x) - T, x0=F_solution, xtol=1e-6)
 
 @deterministic
 def exp_main(F_solution):
+    n_solutions = 200
+    x_list = np.linspace(1, 2e4, n_solutions)
+    F_list = np.empty_like(x_list)
     for i in range(n_solutions):
         # Define function
         F_solution=solve(x_list[i], F_solution)
         F_list[i]=F_solution
+    return F_list, F_solution
 
 @initialize_speedupy
 def main():
