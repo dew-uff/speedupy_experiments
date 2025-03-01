a09e8eed7dd31019be8c1ef9536df618bfab0572
JoaoLopez <joaolopez@id.uff.br>
2025-03-01 20:24:12 -0300

    Adding README.md file for the Power experiment and its updated script.

---
+++ 01pilots/01pilots_exp02_power/README.md
@@ -0,0 +1,16 @@
+# Power Experiment
+
+The five trials used on the memoization techniques article use the same script **power.py** with the following inputs:
+
+- **1241231241 462**
+- **1241231241 450**
+- **1241231000 430**
+- **124123124 400**
+- **1241231231 460**
+
+To execute a trial, type:
+
+```bash
+python speedupy/setup_exp/setup.py power.py
+python power.py 1241231231 460 --exec-mode manual
+```
--- 01pilots/01pilots_exp02_power/power.py
+++ 01pilots/01pilots_exp02_power/power.py
@@ -1,27 +1,20 @@
 import time
 import sys
-
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent / "speedupy"))
-
-
+sys.path.append(str(Path(__file__).parent / 'speedupy'))
 from speedupy.speedupy import initialize_speedupy, deterministic
-
 
 @deterministic
 def pow(n, m):
     if m == 0:
         return 1
-    return n*pow(n, m-1)
-
+    return n * pow(n, m - 1)
 
 @initialize_speedupy
 def main(n, m):
     print(pow(n, m))
-
-
-if __name__ == "__main__":
-    n, m = int(sys.argv[1]), int(sys.argv[2])
+if __name__ == '__main__':
+    (n, m) = (int(sys.argv[1]), int(sys.argv[2]))
     start = time.perf_counter()
     main(n, m)
-    print(time.perf_counter()-start)
+    print(time.perf_counter() - start)
