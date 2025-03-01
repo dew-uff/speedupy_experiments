3def7c8fc5f9944419845723dbe5e084604fcdd2
JoaoLopez <joaolopez@id.uff.br>
2025-03-01 20:18:44 -0300

    Adding README.md file for the Metropolis Hastings experiment and its updated script.

---
+++ 04benchproglangs/04benchpl_exp07_metropolis_hastings/README.md
@@ -0,0 +1,16 @@
+# Metropolis Hastings Experiment
+
+The five trials used on the memoization techniques article use the same script **metropolis_hastings.py** with the following inputs:
+
+- **75000**
+- **72500**
+- **70000**
+- **67500**
+- **65000**
+
+To execute a trial, type:
+
+```bash
+python speedupy/setup_exp/setup.py metropolis_hastings.py
+python metropolis_hastings.py 65000 --exec-mode manual
+```
--- 04benchproglangs/04benchpl_exp07_metropolis_hastings/metropolis_hastings.py
+++ 04benchproglangs/04benchpl_exp07_metropolis_hastings/metropolis_hastings.py
@@ -1,38 +1,34 @@
+import sys
+sys.path.append('/home/joaopedrolopez/Downloads/AvaliacaoExperimental/Experimentos/speedupy_experiments/04benchproglangs/04benchpl_exp07_metropolis_hastings')
+from speedupy.speedupy import maybe_deterministic
 import numpy as np
 import sys
-
 import time
-
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent / "speedupy"))
-
+sys.path.append(str(Path(__file__).parent / 'speedupy'))
 from speedupy.speedupy import initialize_speedupy, deterministic
 
 @deterministic
 def f(x):
-    return np.exp(np.sin(x[0]*5) - x[0]*x[0] - x[1]*x[1])
+    return np.exp(np.sin(x[0] * 5) - x[0] * x[0] - x[1] * x[1])
 
-
-
+@maybe_deterministic
 def markov_chain_function(n):
-    x = np.zeros((2))
+    x = np.zeros(2)
     p = f(x)
     for i in range(n):
-        x2 = x + .01*np.random.randn(x.size)
+        x2 = x + 0.01 * np.random.randn(x.size)
         p2 = f(x2)
-        if (np.random.rand() < (p2/p)):
+        if np.random.rand() < p2 / p:
             x = x2
             p = p2
     return x
 
-
-
 @initialize_speedupy
 def main(n):
     y = markov_chain_function(n)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     N = int(sys.argv[1])
     start = time.perf_counter()
     main(N)
-    print(time.perf_counter()-start)
+    print(time.perf_counter() - start)
