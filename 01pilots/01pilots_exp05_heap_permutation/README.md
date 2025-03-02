bd0f90c281240867aeb92a08a79b0fcf5b728b3a
JoaoLopez <joaolopez@id.uff.br>
2025-03-01 21:01:41 -0300

    Adding README.md file for the Heap Permutation experiment and its updated script.

---
+++ 01pilots/01pilots_exp05_heap_permutation/README.md
@@ -0,0 +1,16 @@
+# Heap Permutation Experiment
+
+The five trials used on the memoization techniques article use the same script **heap_permutation.py** with the following inputs:
+
+- **8**
+- **7**
+- **6**
+- **5**
+- **4**
+
+To execute a trial, type:
+
+```bash
+python speedupy/setup_exp/setup.py heap_permutation.py
+python heap_permutation.py 4 --exec-mode manual
+```
--- 01pilots/01pilots_exp05_heap_permutation/heap_permutation.py
+++ 01pilots/01pilots_exp05_heap_permutation/heap_permutation.py
@@ -1,24 +1,20 @@
 import time
 import sys
-
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent / "speedupy"))
-
+sys.path.append(str(Path(__file__).parent / 'speedupy'))
 from speedupy.speedupy import initialize_speedupy, deterministic
 
 @deterministic
 def heap_permutation(data, n):
     if n == 1:
-        #print(data)
         return
     for i in range(n):
         heap_permutation(data, n - 1)
         if n % 2 == 0:
-            data[i], data[n - 1] = data[n - 1], data[i]
+            (data[i], data[n - 1]) = (data[n - 1], data[i])
         else:
-            data[0], data[n - 1] = data[n - 1], data[0]
+            (data[0], data[n - 1]) = (data[n - 1], data[0])
     return data
-
 
 @initialize_speedupy
 def main(n):
@@ -27,7 +23,6 @@ def main(n):
     heap_permutation(data2, len(data2))
     end_time = time.perf_counter()
     print(end_time - start_time)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     n = int(sys.argv[1])
     main(n)
