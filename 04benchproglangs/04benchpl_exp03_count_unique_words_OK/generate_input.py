ca4b383963385c879a4f33a7689a20cb9a2220fe
JoaoLopez <joao.p.lopez01@gmail.com>
2025-06-30 16:23:11 -0300

    Adding script to automatically generate random input for the Count Unique Words experiment

---
+++ 04benchproglangs/04benchpl_exp03_count_unique_words_OK/generate_input.py
@@ -0,0 +1,5 @@
+import lorem
+
+# print(lorem.sentence())  # One fake sentence
+# print(lorem.paragraph()) # One fake paragraph
+for _ in range(10000): print(lorem.text())      # Multiple paragraphs
