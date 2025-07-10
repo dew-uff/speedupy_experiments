6c70ca5dc0572ef3669d45bc05d3ce83b32401a9
JoaoLopez <joaolopez@id.uff.br>
2025-07-10 00:27:19 -0300

    Removing old inputs for the Count Unique Words experiment

--- 04benchproglangs/04benchpl_exp03_count_unique_words_OK/generate_input.py
+++
@@ -1,5 +0,0 @@
-import lorem
-
-# print(lorem.sentence())  # One fake sentence
-# print(lorem.paragraph()) # One fake paragraph
-for _ in range(10000): print(lorem.text())      # Multiple paragraphs
